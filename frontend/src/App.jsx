2c570a7b31e54c82d9ee1da1f26e51266cc5e749
sumit kumar mahto <sumit.mahto9131@gmail.com>
2025-12-08 21:37:09 +0530

    Added About Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
+import AboutUs from './pages/AboutUs';
 import { ToastProvider } from './context/ToastContext';
 
 const App = () => {
   return (
     <ToastProvider>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} />c
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </ToastProvider>
   );

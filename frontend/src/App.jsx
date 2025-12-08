18139b3eee095dc26d92bb4c02eb482d0e980fa5
Ojasdudooknale <ojasdudooknale@gmail.com>
2025-12-08 23:25:21 +0530

    refactor:added movie-details route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import { ToastProvider } from './context/ToastContext';
 import ErrorPage from './pages/ErrorPage';
 import UserView from './pages/UserView';
+import MovieDetailPage from './pages/MovieDetailPage';
 const App = () => {
   return (
     <ToastProvider>
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/userview" element={<UserView />} />
+        <Route path="/movie/:id" element={<MovieDetailPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </ToastProvider>
